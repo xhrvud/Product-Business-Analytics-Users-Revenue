e6c7c9a220ff4c68b77322dfffc401a4443af5a7
Levytskyi Bohdan <xhrvud@gmail.com>
2026-02-01 12:40:01 +0100

    Update 01_schema.sql

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,1 +1,13 @@
+-- Table schema for raw e-commerce events data
 
+CREATE TABLE events (
+    event_time TIMESTAMP,
+    event_type TEXT,
+    product_id BIGINT,
+    category_id BIGINT,
+    category_code TEXT,
+    brand TEXT,
+    price NUMERIC,
+    user_id BIGINT,
+    user_session TEXT
+);
