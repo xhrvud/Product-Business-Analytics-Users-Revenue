e38a1b86d82fc19f350e219d4c16c214f17dcad6
Levytskyi Bohdan <xhrvud@gmail.com>
2026-02-01 12:40:16 +0100

    Update 02_indexes.sql

--- sql/02_indexes.sql
+++ sql/02_indexes.sql
@@ -1,1 +1,6 @@
+-- Indexes to optimize analytical queries on large datasets
 
+CREATE INDEX idx_events_time ON events(event_time);
+CREATE INDEX idx_events_user ON events(user_id);
+CREATE INDEX idx_events_session ON events(user_session);
+CREATE INDEX idx_events_type ON events(event_type);
